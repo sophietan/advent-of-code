f5362113c4fed48273dac1664342b4d0ff34f5b6
Sophie Tan <sophiemltan@gmail.com>
2021-02-21 18:56:13 +0000

    Finish day 8

--- y2020/day8.py
+++ y2020/day8.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 from typing import List
 
 
@@ -6,31 +5,51 @@ class Game:
     def __init__(self, instructions: List[str]):
         self.instructions = instructions
         self.position = 0
+        self.prev_position = 0
         self.score = 0
+        self.seen = set()
 
     def acc(self, arg: int):
         self.score += arg
+        self.seen.add(self.position)
+        self.prev_position = self.position
         self.position += 1
 
     def jmp(self, arg: int):
+        self.seen.add(self.position)
+        self.prev_position = self.position
+        self.position += arg
+
+    def nop(self, arg: int):
+        self.seen.add(self.position)
+        self.prev_position = self.position
         self.position += 1
 
-    def nop(self):
-        self.position += 1
+    def reset(self):
+        self.position = 0
+        self.seen = set()
+        self.prev_position = 0
+        self.score = 0
 
     def process(self):
-        for instruction in self.instructions:
+        while self.position not in self.seen and self.position < len(self.instructions):
             instruction = self.instructions[self.position]
             i, arg = instruction.strip().split()
             op = getattr(self, i)
             op(int(arg))
+        else:
+            if self.position < len(self.instructions):
+                old = self.instructions[self.prev_position]
+                self.instructions[self.prev_position] = old.replace('jmp', 'nop')
+                self.reset()
+                self.process()
+            print(self.score)
+
 
 def main():
-    with open('y2020/inputs/day8.txt', 'r') as f:
+    with open('../y2020/inputs/day8.txt', 'r') as f:
         g = Game(f.readlines())
         g.process()
-        # pprint(f.readlines())
-
 
 
 if __name__ == '__main__':
